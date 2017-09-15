e3a350d9142dc3d709c4bd9179d118dfc25ac4ce
Erwan <pelmoine.erwan@gmail.com>
2017-09-15 15:05:39 +0200

    add comment
    add comment

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,11 @@
 
 ## Write a short comment describing this function
 
+## this function is composed by 4 functions
+## set : to set your data 
+## get : to get your data
+## setSolve : to set your inverse matrix
+## getSolve : to get yout inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
 	m <- NULL
 	set <- function(y) {
@@ -18,6 +23,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
+## This function put the inverse matrix in cache and return it.
+## If it's already in cache it's return immediately.
 cacheSolve <- function(x, ...) {
 	m <- x$getSolve()
   	if(!is.null(m)) {
